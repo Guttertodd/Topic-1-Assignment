b71f0a3304ff4a46446967060c2679f44eed90ce
339713273 <339713273@2260I-L29-026>
2024-09-06 09:41:18 -0400

    Done

--- Topic 1 Assignment/Program.cs
+++ Topic 1 Assignment/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Mr Aldworth!"); //Carter Todd
+
+            Console.WriteLine("Comments are used to send hidden messages to yourself or other programers, also to rememer where you left off if you are going to test something or do something risky,");
+
+            Console.WriteLine(" CCC   TTTTT");
+            Console.WriteLine("C   C    T  ");
+            Console.WriteLine("C        T  ");
+            Console.WriteLine("C        T  ");
+            Console.WriteLine("C        T  ");
+            Console.WriteLine("C   C    T  ");
+            Console.WriteLine(" CCC     T  ");
+           // This is an unnecessary comment 
+             
         }
     }
 }
